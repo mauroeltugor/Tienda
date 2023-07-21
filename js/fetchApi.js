2159945e05de6fae3fccd5cfb2d627a64a8cbeee
mauro <andrekxus299@gmail.com>
2023-07-21 07:28:48 -0500

    finish all e-comerce

--- js/fetchApi.js
+++ js/fetchApi.js
@@ -1,9 +1,8 @@
-const api = 'https://fakestoreapi.com/products';
-const cardSection = document.getElementById('card');
-const template = document.getElementById('template');
-const cantidad = document.getElementById('cantidad');
-const total = document.getElementById('total');
-const button = document.getElementById('boton');
+const api = "https://fakestoreapi.com/products";
+const cardSection = document.getElementById("card");
+const template = document.getElementById("template");
+const quantityElement = document.getElementById("quantity");
+const totalElement = document.getElementById("total");
 
 const cart = [];
 
@@ -14,34 +13,53 @@ const getData = async (url) => {
         image: product.image,
         name: product.title,
         price: product.price,
-        category: product.category
+        category: product.category,
     }));
     return finalData;
-}
-
-// const addProduct = () => {
-//     cart.push(1);
-//     total.textContent = cart.length;
-// }
-
-// const ss = button.addEventListener('click', addProduct);
+};
 
 const fullCard = async (newUrl) => {
     const allProducts = await getData(newUrl);
     const fragment = document.createDocumentFragment();
     allProducts.forEach((product) => {
         const card = template.content.cloneNode(true);
-        const image = card.getElementById('imagenProducto');
-        const nombre = card.getElementById('nombreProducto');
-        const precio = card.getElementById('precioProducto');
-        const categoria = card.getElementById('categoriaProducto');
+        const image = card.getElementById("productImage");
+        const name = card.getElementById("productName");
+        const price = card.getElementById("productPrice");
+        const category = card.getElementById("productCategory");
+        const button = card.querySelector(".button");
+
         image.src = product.image;
-        nombre.textContent = product.name;
-        precio.textContent = product.price;
-        categoria.textContent = product.category;
+        name.textContent = product.name;
+        price.textContent = product.price;
+        category.textContent = product.category;
+
+        button.addEventListener("click", () => addToCart(product.name, product.price));
+
         fragment.appendChild(card);
     });
     cardSection.appendChild(fragment);
-}
+};
+
+const addToCart = (name, price) => {
+    let product = {
+        name: name,
+        price: price,
+    };
+
+    cart.push(product);
+    updateCart();
+};
+
+const updateCart = () => {
+    let total = 0;
+    let quantity = cart.length;
+    for (let i = 0; i < cart.length; i += 1) {
+        total += cart[i].price;
+    }
+
+    quantityElement.innerText = "Quantity of products: " + quantity;
+    totalElement.innerText = "Total to pay: " + total.toFixed(2);
+};
 
 fullCard(api);
